817e14eaeb556e4184b3a27f9387023f9ecd8302
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2025-03-03 14:32:07 +0100

    feat: Added ibat current measurement and conversion

--- examples/nrf9160/src/bin/configure-charging.rs
+++ examples/nrf9160/src/bin/configure-charging.rs
@@ -10,10 +10,20 @@ use embassy_nrf::{
     twim::{self, Twim},
 };
 
+use embassy_time::Timer;
+
 use {defmt_rtt as _, panic_probe as _};
 
 use npm1300_rs::{
     NtcThermistorType,
+    charger::{
+        ChargerTerminationVoltage,
+        DischargeCurrentLimit,
+        ChargerTerminationCurrentLevelSelect,
+    },
+    sysreg::{
+        VbusInCurrentLimit,
+    },
     NPM1300,
 };
 
@@ -37,13 +47,33 @@ async fn main(_spawner: Spawner) {
     let twi = Twim::new(p.SERIAL0, Irqs, sdapin, sclpin, config);
     
     let mut npm1300 = NPM1300::new(twi, embassy_time::Delay);
+    
+    let _ = npm1300.set_vbus_in_current_limit(VbusInCurrentLimit::MA1000).await;
+
+    
     defmt::info!("Configuring NTC Resistor...");
     let _ = npm1300.configure_ntc_resistance(NtcThermistorType::Ntc10K, Some(3380.0)).await;
+    let _ = npm1300.use_ntc_measurements().await;
 
-    let vbat_measurement = npm1300.measure_vbat().await.unwrap();
-    defmt::info!("VBAT: {=f32} mV", vbat_measurement);
-    
-    let _ = npm1300.measure_ntc().await;
-    let ntc_temp = npm1300.get_ntc_measurement_result().await.unwrap();
-    defmt::info!("NTC Temp: {=f32} C", ntc_temp);
+    defmt::info!("Configuring Charging...");
+    let _ = npm1300.set_charger_current(300).await;
+    let _ = npm1300.set_termination_current_level(ChargerTerminationCurrentLevelSelect::SEL10).await;
+    let _ = npm1300.set_normal_temperature_termination_voltage(ChargerTerminationVoltage::V4_20).await;
+    let _ = npm1300.set_warm_temperature_termination_voltage(ChargerTerminationVoltage::V4_10).await;
+    let _ = npm1300.set_discharge_current_limit(DischargeCurrentLimit::Low).await;
+    let _ = npm1300.enable_battery_charging().await;
+    let _ = npm1300.enable_battery_recharge().await;
+    let _ = npm1300.configure_ibat_measurement(true).await;
+
+    loop{
+        let vbat_voltage = npm1300.measure_vbat().await.unwrap();
+        let _ = npm1300.measure_ntc().await;
+        let ntc_temp = npm1300.get_ntc_measurement_result().await.unwrap();
+        let die_temp = npm1300.measure_die_temperature().await;
+        defmt::info!("Setup: {:?}", npm1300.get_charger_status().await.unwrap());
+        defmt::info!("VBAT: {=f32} mV, NTC Temp: {=f32}, Die Temp: {:?} C", vbat_voltage, ntc_temp, die_temp);
+        let ibat_current = npm1300.measure_ibat().await.unwrap();
+        defmt::info!("IBAT Current: {:?}", ibat_current);
+        Timer::after_millis(1000).await;
+    }
 }
