5158472a4da6bca7dc101f9d948058fcfecc6493
Fredrik Beilegaard <fredrik.kleven@outlook.com>
2025-08-11 14:40:07 +0200

    Fix: Cleanup

--- src/charger/mod.rs
+++ src/charger/mod.rs
@@ -5,7 +5,7 @@ pub use types::*;
 
 use libm::roundf;
 
-use crate::{common::Task, Bchgilimbatactive, Dietemphigh, Ibatmeasenable,NPM1300Error};
+use crate::{common::Task, Bchgilimbatactive, Dietemphigh, NPM1300Error};
 
 impl<I2c: embedded_hal_async::i2c::I2c, Delay: embedded_hal_async::delay::DelayNs>
     crate::NPM1300<I2c, Delay>
